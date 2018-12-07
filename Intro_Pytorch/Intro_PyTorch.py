3ae283b1362b7714d8d9e3cb6a3c27bab09aef09
siddhantb <siddhantb@dosepack.com>
2018-12-07 07:53:12 +0530

    Explore linear and matrix multiplication in PyTorch and time them

--- Intro_Pytorch/Intro_PyTorch.py
+++ Intro_Pytorch/Intro_PyTorch.py
@@ -3,6 +3,7 @@ This file contains the code written while learning about PyTorch.
 """
 # Importing PyTorch
 import torch
+import time
 
 
 def sigmoid_activation(x):
@@ -13,3 +14,21 @@ def sigmoid_activation(x):
     """
     return 1/(1 + torch.exp(-x))
 
+
+# Generating some data
+torch.manual_seed(7)  # Setting the random seed
+# Features
+features = torch.randn((1, 5))
+# Random weights
+weights = torch.rand_like(features)
+# Bias value
+bias = torch.randn((1, 1))
+
+s = time.time()
+ans = torch.sum(features*weights) + bias
+print('Answer without matrix multiplication is {}, time taken is {}.'.format(ans, time.time()-s))
+
+a = time.time()
+weights = weights.view(5, 1)
+ans_mat = torch.sum(torch.mm(features, weights)) + bias
+print('Answer without matrix multiplication is {}, time taken is {}.'.format(ans_mat, time.time()-a))
