7da3f4b6635c477e348b46581ee4235c7c7004a3
siddhantb <siddhantb@dosepack.com>
2018-12-16 13:44:45 +0530

    Add new file Inference_validation.py

---
+++ Intro_Pytorch/Inference_Validation.py
@@ -0,0 +1,125 @@
+from torchvision import datasets, transforms
+from helper import *
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torch import optim, nn
+import torch
+
+# Define transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5),)
+                                ])
+# Download and load the training data
+train_set = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, transform=transform, train=True)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
+
+# Download and load the test data
+test_set = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=True)
+
+
+class Classifier(nn.Module):
+    """This is the model that we'll train"""
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+        # Dropout module with 0.2 drop probability
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        # Flattening the input tensor
+        x = x.view(x.shape[0], -1)
+
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+
+        x = F.log_softmax(self.fc4(x), dim=1)
+
+        return x
+
+
+# model = Classifier()
+#
+# images, labels = next(iter(test_loader))
+# # Get the class probabilities
+# ps = torch.exp(model(images))
+# # print(ps.shape)
+#
+#
+# top_p, top_class = ps.topk(1, dim=1)
+# # print(top_class[:10, :])
+#
+# equals = top_class == labels.view(*top_class.shape)
+#
+# accuracy = torch.mean(equals.type(torch.FloatTensor))
+# print(f'Accuracy: {accuracy.item()*100}%')
+
+model = Classifier()
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+epochs = 30
+steps = 0
+
+train_losses, test_losses = list(), list()
+
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+
+        log_ps = model(images)
+        loss = criterion(log_ps, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    else:
+        test_loss = 0
+        accuracy = 0
+
+        # Turn of gradients
+        with torch.no_grad():
+            for images, labels in test_loader:
+                log_ps = model(images)
+                test_loss += criterion(log_ps, labels)
+
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+        train_losses.append(running_loss/len(train_loader))
+        test_losses.append(test_loss/len(test_loader))
+
+        print('Epoch: {}/{}...'.format(e+1, epochs),
+              'Training Loss: {:.3f}...'.format(running_loss/len(train_loader)),
+              'Test Loss: {:.3f}...'.format(test_loss/len(test_loader)),
+              'Test Accuracy: {:.3f}...'.format(accuracy/len(test_loader)))
+
+# Plot the results
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.legend(frameon=False)
+plt.show()
+
+model.eval()
+
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+img = images[0]
+
+img = img.view(1, 784)
+
+with torch.no_grad():
+    output = model.forward(img)
+
+ps = torch.exp(output)
+
+view_classify(img.view(1, 28, 28), ps, version='Fashion')
---
+++ q
@@ -0,0 +1,47 @@
+* [33mcommit b353660980d67681895c384aa8c69f5e05dc1d3c[m
+[31m|[m Author: siddhantb <siddhantb@dosepack.com>
+[31m|[m Date:   Sun Dec 16 09:17:33 2018 +0530
+[31m|[m 
+[31m|[m     Print some extra information
+[31m|[m  
+* [33mcommit 2c2304cd6079ac13b3f14e654d92713bd7171aa9[m
+[31m|[m Author: siddhantb <siddhantb@dosepack.com>
+[31m|[m Date:   Sun Dec 16 09:12:14 2018 +0530
+[31m|[m 
+[31m|[m     Add new file fashion_MNIST.py
+[31m|[m  
+* [33mcommit bb47d97a7f4867a8b746a06861185e8892150bd5[m
+[31m|[m Author: siddhantb <siddhantb@dosepack.com>
+[31m|[m Date:   Sat Dec 15 22:57:37 2018 +0530
+[31m|[m 
+[31m|[m     Add Training_NN.py
+[31m|[m  
+* [33mcommit c546d7546273eb589864c3c73d5f6ba42356867f[m
+[31m|[m Author: siddhantb <siddhantb@dosepack.com>
+[31m|[m Date:   Sat Dec 15 16:28:48 2018 +0530
+[31m|[m 
+[31m|[m     Learn about creating a NN in PyTorch
+[31m|[m  
+* [33mcommit d0f905a62d26373411b923af0da6b30d263cf52d[m
+[31m|[m Author: siddhantb <siddhantb@dosepack.com>
+[31m|[m Date:   Sat Dec 15 12:51:16 2018 +0530
+[31m|[m 
+[31m|[m     Add new file
+[31m|[m  
+* [33mcommit 75a6f9138ab92713589b716f771c19f1a5209def[m
+[31m|[m Author: siddhantb <siddhantb@dosepack.com>
+[31m|[m Date:   Fri Dec 7 20:20:07 2018 +0530
+[31m|[m 
+[31m|[m     Solve exercise of creating a small NN
+[31m|[m  
+* [33mcommit 3ae283b1362b7714d8d9e3cb6a3c27bab09aef09[m
+[31m|[m Author: siddhantb <siddhantb@dosepack.com>
+[31m|[m Date:   Fri Dec 7 07:53:12 2018 +0530
+[31m|[m 
+[31m|[m     Explore linear and matrix multiplication in PyTorch and time them
+[31m|[m  
+* [33mcommit c4763d91adfb856c5a08a5c666ea572f9ea86286[m
+  Author: siddhantb <siddhantb@dosepack.com>
+  Date:   Fri Dec 7 07:30:02 2018 +0530
+  
+      Initial Commit
